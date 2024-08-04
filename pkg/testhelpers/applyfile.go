e4b8219c03c0eb2ce6d528ff1bcf1e7ad166485e
Barak Korren <bkorren@redhat.com>
2024-08-04 16:02:56 +0300

    feat(RHTAPWATCH-1192): Server-Side-Apply template
    Use server-side-apply for templated objects instead of create or update
    operations. This causes fields on existing resources that do not have
    values sepcified in the template to be preserved.
    
    The make the controller not clobber fields such as `spec.conteinarImage`
    and `metadata.finalizers` that are set by other controllers uless
    values are explicitly set in the template.

--- pkg/testhelpers/applyfile.go
+++ pkg/testhelpers/applyfile.go
@@ -5,24 +5,14 @@ import (
 
 	g "github.com/onsi/gomega"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func ApplyFile(ctx context.Context, k8sClient client.Client, path string, ns string) {
-	var res, fileRes *unstructured.Unstructured
-	fileRes = new(unstructured.Unstructured)
-	ResourceFromFile(path, fileRes)
-	fileRes.SetNamespace(ns)
-	// Keep a clean copy of the data from the file
-	res = fileRes.DeepCopy()
-	err := k8sClient.Create(ctx, res)
-	if !apierrors.IsAlreadyExists(err) {
-		g.Expect(err).NotTo(g.HaveOccurred())
-		return
-	}
-	res = fileRes.DeepCopy()
+	res := new(unstructured.Unstructured)
+	ResourceFromFile(path, res)
+	res.SetNamespace(ns)
 	g.Expect(k8sClient.Patch(
 		ctx, res,
 		client.Apply,
